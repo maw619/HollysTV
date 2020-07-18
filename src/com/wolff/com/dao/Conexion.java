d81675c029ace27de95f5ac99cf3c0080a47e14b
Marco <marcowolff619@gmail.com>
2020-07-18 14:12:03 -0400

    fixed the reload svc

--- src/com/wolff/com/dao/Conexion.java
+++ src/com/wolff/com/dao/Conexion.java
@@ -12,27 +12,28 @@ public class Conexion {
 
 	
 	public void getConn() {
-		String url = "jdbc:mysql://127.0.0.1:3306/holly";
-
-		String user = "root";
 		
-		String password = "";
+		  String url = "jdbc:mysql://127.0.0.1:3306/holly";
+		  
+		  String user = "root";
+		  
+		  String password = "";
+		 
 		
 		
-//		String user = "hollystv_mwolff2";
-//		String password = "Holly";
-//		String dbHost = "mysql3000.mochahost.com";
-//		String dbName = "hollystv_db2";
-//		String url ="jdbc:mysql://"+dbHost+":3306/"+dbName;
-		
-		try {
-			Class.forName("com.mysql.jdbc.Driver");
-			conn = DriverManager.getConnection(url,user,password);
-			System.out.println("connected");
-		}catch(SQLException | ClassNotFoundException e) {
- 			e.printStackTrace();
-		}
-		
+			/*
+			 * String user = "hollystv_mwolff2"; String password = "Holly"; String dbHost =
+			 * "mysql3000.mochahost.com"; String dbName = "hollystv_db2"; String url
+			 * ="jdbc:mysql://"+dbHost+":3306/"+dbName;
+			 */
+		  
+		  
+		  
+		  try { Class.forName("com.mysql.jdbc.Driver"); conn =
+		  DriverManager.getConnection(url,user,password);
+		  System.out.println("connected"); }catch(SQLException | ClassNotFoundException
+		  e) { e.printStackTrace(); }
+		 
 	}
 
 
